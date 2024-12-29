5abfa9ee0ea4f1d81f3182df1a0066bd9c8ad30a
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-12-29 17:54:36 -0500

    Added logic to convert exceptions into Blueprint exception into

---
+++ Source/RetroLibUE/Public/RetroLib/Exceptions/ConvertException.h
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include <Blueprint/BlueprintExceptionInfo.h>
+#include <exception>
+
+namespace Retro {
+	inline FBlueprintExceptionInfo ConvertException(const std::exception& Exception, EBlueprintExceptionType::Type Type = EBlueprintExceptionType::AccessViolation) {
+		return FBlueprintExceptionInfo(Type, FText::FromString(Exception.what()));
+	}
+}
